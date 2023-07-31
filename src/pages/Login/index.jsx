7d28192ab531b588406997361c392d8842e585cb
ecampusano <ecampusano@dgcp.gob.do>
2023-07-30 20:23:50 -0400

    console log

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,6 +5,7 @@ import { AuthConsumer } from '../../auth';
 
 function Login() {
     const [authed, dispatch] = AuthConsumer();
+    console.log(authed)
     return (
         <div id="login">
             <Form type="login" />
